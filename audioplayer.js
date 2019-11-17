8fa3e9360550d65efdd8c9c97e0c01e2458ee209
Yunige Woo <yunigewoo@Yuniges-MacBook-Air.local>
2019-11-18 00:37:38 +0900

    플레이어 수정

--- audioplayer.js
+++ audioplayer.js
@@ -1,105 +1,52 @@
-var music = document.getElementById('music'); // id for audio element
-var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
-var pButton = document.getElementById('pButton'); // play button
-var playhead = document.getElementById('playhead'); // playhead
-var timeline = document.getElementById('timeline'); // timeline
+$(function() {
 
-// timeline width adjusted for playhead
-var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+	var audio = $("audio")[0];
 
-// play button event listenter
-pButton.addEventListener("click", play);
+	$('#btn-play-pause').on('click', function() {
+		//Play/pause the track
+		if (audio.paused == false) {
+			audio.pause();
+			$(this).children('i').removeClass('fa-pause');
+			$(this).children('i').addClass('fa-play');
+		} else {
+			audio.play();
+			$(this).children('i').removeClass('fa-play');
+			$(this).children('i').addClass('fa-pause');
+		}
+	});
 
-// timeupdate event listener
-music.addEventListener("timeupdate", timeUpdate, false);
+	$('#btn-stop').on('click', function() {
+		//Stop the track
+		audio.pause();
+		audio.currentTime = 0;
+		$('#btn-play-pause').children('i').removeClass('fa-pause');
+		$('#btn-play-pause').children('i').addClass('fa-play');
+	});
 
-// makes timeline clickable
-timeline.addEventListener("click", function(event) {
-    moveplayhead(event);
-    music.currentTime = duration * clickPercent(event);
-}, false);
+	$('#btn-mute').on('click', function() {
+		//Mutes/unmutes the sound
+		if(audio.volume != 0) {
+			audio.volume = 0;
+			$(this).children('i').removeClass('fa-volume-off');
+			$(this).children('i').addClass('fa-volume-up');
+		} else {
+			audio.volume = 1;
+			$(this).children('i').removeClass('fa-volume-up');
+			$(this).children('i').addClass('fa-volume-off');
+		}
+	});
 
-// returns click as decimal (.77) of the total timelineWidth
-function clickPercent(event) {
-    return (event.clientX - getPosition(timeline)) / timelineWidth;
-}
+	function updateProgress() {
+		//Updates the progress bar
+		var progress = document.getElementById("progress");
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
+		}
+		progress.style.width = value + "%";
+	}
 
-// makes playhead draggable
-playhead.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
+	//Progress Bar event listener
+	audio.addEventListener("timeupdate", updateProgress, false);
 
-// Boolean value so that audio position is updated only when the playhead is released
-var onplayhead = false;
-
-// mouseDown EventListener
-function mouseDown() {
-    onplayhead = true;
-    window.addEventListener('mousemove', moveplayhead, true);
-    music.removeEventListener('timeupdate', timeUpdate, false);
-}
-
-// mouseUp EventListener
-// getting input from all mouse clicks
-function mouseUp(event) {
-    if (onplayhead == true) {
-        moveplayhead(event);
-        window.removeEventListener('mousemove', moveplayhead, true);
-        // change current time
-        music.currentTime = duration * clickPercent(event);
-        music.addEventListener('timeupdate', timeUpdate, false);
-    }
-    onplayhead = false;
-}
-// mousemove EventListener
-// Moves playhead as user drags
-function moveplayhead(event) {
-    var newMargLeft = event.clientX - getPosition(timeline);
-
-    if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-        playhead.style.marginLeft = newMargLeft + "px";
-    }
-    if (newMargLeft < 0) {
-        playhead.style.marginLeft = "0px";
-    }
-    if (newMargLeft > timelineWidth) {
-        playhead.style.marginLeft = timelineWidth + "px";
-    }
-}
-
-// timeUpdate
-// Synchronizes playhead position with current point in audio
-function timeUpdate() {
-    var playPercent = timelineWidth * (music.currentTime / duration);
-    playhead.style.marginLeft = playPercent + "px";
-    if (music.currentTime == duration) {
-        pButton.className = "";
-        pButton.className = "play";
-    }
-}
-
-//Play and Pause
-function play() {
-    // start music
-    if (music.paused) {
-        music.play();
-        // remove play, add pause
-        pButton.className = "";
-        pButton.className = "pause";
-    } else { // pause music
-        music.pause();
-        // remove pause, add play
-        pButton.className = "";
-        pButton.className = "play";
-    }
-}
-
-// Gets audio file duration
-music.addEventListener("canplaythrough", function() {
-    duration = music.duration;
-}, false);
-
-// getPosition
-// Returns elements left position relative to top-left of viewport
-function getPosition(el) {
-    return el.getBoundingClientRect().left;
-}
+});
